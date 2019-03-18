37c0a4e2a032bd4b28f189d9c5ff64fb78082da9
Tara-Gabriella Engelhardt <taraengy@gmail.com>
2019-03-18 18:52:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+  let hq = 42
+    return blocks - hq
+}
+
+
+function distanceFromHqInFeet(hqFeet) {
+  
+}
+
+function distanceTravelledInFeet(start, end) {
+  
+}
+
+function calculatesFarePrice(start, end) {
+  
+}
